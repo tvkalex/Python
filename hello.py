f0d1af81a170ce2741dbcfea6b4fb545e3676b77
tvkalex <tvkalex@yandex.ru>
2022-12-12 13:04:07 +0300

    Add HelloPython

---
+++ hello.py
@@ -0,0 +1,19 @@
+print('Hello Мир')
+
+# типы данных и переменная
+# int, float, boolean, str, list, None
+value = None
+print(type(value))
+
+a = 123
+b = 1.23
+value = 12334
+print(a)
+print(b)
+print(value)
+print(type(a))
+print(type(b))
+print(type(value))
+
+s = 'hello world'
+print(s) # вывод строки
