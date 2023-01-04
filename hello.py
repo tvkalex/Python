a2bc715283bd09847d47cb7506eac6b976541b20
tvkalex <tvkalex@yandex.ru>
2023-01-04 16:51:31 +0300

    Add hello.py

--- hello.py
+++ hello.py
@@ -2,18 +2,66 @@ print('Hello Мир')
 
 # типы данных и переменная
 # int, float, boolean, str, list, None
-value = None
-print(type(value))
+# value = None
+# print(type(value))
 
 a = 123
 b = 1.23
-value = 12334
-print(a)
-print(b)
-print(value)
-print(type(a))
-print(type(b))
-print(type(value))
+# value = 12334
+# # print(a)
+# print(b)
+# print(value)
+# print(type(a))
+# print(type(b))
+# print(type(value))
 
 s = 'hello world'
-print(s) # вывод строки
+# print(s)
+# s = "hello 'world"
+# print(s)
+# s = 'hello "world'
+# print(s)
+# s = 'hello \'world'
+# print(s)
+# s = 'hello \nworld'
+# print(s) # вывод строки
+
+# print(a,'-',b,'-',s)
+# print('{} - {} - {}'. format(a, b, s))
+# print(f'{a} - {b} - {s}')
+# print('{1} - {2} - {0}'. format(a, b, s))
+
+# f = True
+# print(f)
+
+# list = [] # как массив в c#
+# list = [1,2,3]
+# list = ['1','2','3','hello',1,2,3, True] # можно, но лучше не хранить в массиве разные типы данных
+# print(list)
+
+# print('Введите а')
+# a = input()
+# print('Введите b')
+# b = input()
+# print(a, b)
+# print('{} {}'.format(a, b))
+# print(f'{a} {b}')
+
+# print('Введите а')
+# a = int(input())
+# print('Введите b')
+# b = int(input())
+# print(a, '+', b, '=', a+b)
+
+print('Введите а')
+a = float(input())
+print('Введите b')
+b = float(input())
+print(a, '+', b, '=', a+b)
+
+# Арифметические операции
+# +, -, *, /, %, //, **
+# **, ⊕, ⊖, *, /, //, %, +, -
+# () , Сокращённые операции
+a = 123
+b = 1
